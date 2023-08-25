f2eb71e35eba2754ccaf0351c0241826aaf41729
unknown <jschueren1431@gmail.com>
2023-08-24 20:01:39 -0700

    updated favicon and title for the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Joe Schueren | Software Developer</title>
         <script src="https://kit.fontawesome.com/a62fd80284.js" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>
