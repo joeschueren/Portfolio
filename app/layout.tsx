c1fb6318ea381665c0061f6d9a991a1a7622b129
unknown <jschueren1431@gmail.com>
2023-10-27 12:27:58 -0700

    added analytics to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="container">
           {children}
+          <Analytics />
         </div>
           </body>
     </html>
